619049d006eba4ad8efcabc8f75369c95649f809
xinde.yang <xinde.yang@datayes.com>
2022-01-19 17:14:36 +0800

    feat:upload

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ createFolder(uploadFolder);
 //实例化multer，传递的参数对象，dest表示上传文件的存储路径
 let objMulter = multer({ dest: path.join(__dirname, "./upload") });
 app.use(objMulter.any()); //any表示任意类型的文件
-// // app.use(objMulter.image())//仅允许上传图片类型
+// app.use(objMulter.image())//仅允许上传图片类型
 // app.use("/", express.static("./upload")); //将静态资源托管，这样才能在浏览器上直接访问预览图片或则html页面
 app.use(bodyParser.urlencoded({ extended: true }));
 
